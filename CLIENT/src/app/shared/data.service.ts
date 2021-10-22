b745c77d4f9cf348e48be4a9eaefadf5bf551d58
sajjadTareen <sajjad.urrehman@systemsltd.com>
2021-10-22 15:47:10 +0500

    Integrated Update Todo

--- CLIENT/src/app/shared/data.service.ts
+++ CLIENT/src/app/shared/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +13,7 @@ export class DataService {
     // new Todo('Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptates dolorem alias dolores deserunt, qui, amet odio facilis tempora unde sequi numquam explicabo nihil iste labore beatae ea rerum expedita.', true)
   ]
   
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getAllTodos() {
     let url= "http://localhost:3000/"
@@ -22,11 +24,9 @@ export class DataService {
     //this.todos.push(todo)
   }
 
-  updateTodo(updatedTodo: Todo) {
+  updateTodo(updatedTodo: any) {
     let url= "http://localhost:3000/"
-    const body = updatedTodo;
-    console.log(updatedTodo);
-    return this.http.put(url,body);
+      return this.http.put(url, updatedTodo);
   }
 
   deleteTodo(index: number) {
--- server/app.js
+++ server/app.js
@@ -9,14 +9,14 @@ app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
-    next();
+        next();
   });
 
 const todoRoutes = require('./routes/todo');
 const Todo = require('./models/todo');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', todoRoutes);
